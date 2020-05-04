6882c56a07183a66d205681c7a14d1f41481dc19
iAquino47 <aquinoian@icloud.com>
2020-05-04 15:02:13 +0100

    added adding and cancelling bookings functionality

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const Event = require('../../models/event');
 const User = require('../../models/user');
+const Booking = require('../../models/booking');
 
 module.exports = {
     events: async () => {
@@ -16,6 +17,21 @@ module.exports = {
                 })
         } catch (error) {
             throw error
+        }
+    },
+    bookings: async () => {
+        try {
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return {
+                    ...booking._doc, 
+                    _id: booking.id, 
+                    createdAt: new Date(booking._doc.createdAt).toISOString(),
+                    updatedAt: new Date(booking._doc.createdAt).toISOString()
+                };
+            });
+        } catch (err) {
+            throw err;
         }
     },
     createEvent: async args => {
@@ -67,5 +83,35 @@ module.exports = {
         } catch (error) {
             throw error;
         }
+    },
+    bookEvent: async args => {
+        const fetchedEvent = await Event.findOne({_id: args.eventId});
+        const booking = new Booking({
+            user: '5ea0c1a28deb2ce31608ea14',
+            event: fetchedEvent
+        });
+        const result = await booking.save();
+        return {
+            ...result._doc,
+            _id: result._id,
+            user: booking._doc.user,
+            event: booking._doc.event,
+            createdAt: new Date(result._doc.createdAt).toISOString(),
+            updatedAt: new Date(result._doc.createdAt).toISOString()
+        }
+    },
+    cancelBooking: async args => {
+        try {
+            const booking =  await (await Booking.findById(args.bookingId)).populate('event');
+            const event = {
+                ...booking.event._doc, 
+                _id: booking.event.id
+            };
+            await Booking.deleteOne({_id: args.bookingId});
+            return event;
+        } catch (err) {
+            throw err;
+        }
     }
 }
+
