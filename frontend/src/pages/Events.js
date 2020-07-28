82af91005cb5fd481876ec5d06869d7fd5c5d0e2
iAquino47 <aquinoian@icloud.com>
2020-07-28 14:24:49 +0100

    basic single page setup with router

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class EventsPage extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>The Events Page</h1> );
+    }
+}
+ 
+export default EventsPage;
